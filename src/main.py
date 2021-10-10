40eed692d012c752a68204f7ce672936b35e1f74
David Delgado-Dueñas <35778364+Ddelgadu@users.noreply.github.com>
2021-10-10 13:42:33 +0200

    Small Changes

--- src/main.py
+++ src/main.py
@@ -10,14 +10,14 @@ import tradesAndOrders as tao
 async def main():
     client = await AsyncClient.create(api_key=api_key, api_secret=api_secret)
 
-    #await get_assets_snap(client)
-    #await get_candles(client)
-    #await tao.get_dividends(client)
-    #await tao.get_dust(client)
+    await get_assets_snap(client)
+    await get_candles(client)
+    await tao.get_dividends(client)
+    await tao.get_dust(client)
     await tao.get_ord_and_trad(client, 0)
     await tao.get_ord_and_trad(client, 1)
-    #await tao.get_fiat_orders()
-    #await tao.get_fiat_dep_withdraws(is_withdraw=1)
+    await tao.get_fiat_orders()
+    await tao.get_fiat_dep_withdraws(is_withdraw=1)
     await tao.get_fiat_dep_withdraws(is_withdraw=0)
     await client.close_connection()
 
