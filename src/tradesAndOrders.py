6971c7a80f234c665593832855d64c49df6304da
David Delgado-Dueñas <35778364+Ddelgadu@users.noreply.github.com>
2021-10-10 15:27:35 +0200

    deposits and withdraws crypto

--- src/tradesAndOrders.py
+++ src/tradesAndOrders.py
@@ -174,7 +174,6 @@ async def get_dep_with(client, is_deposit=1):
         for op in div:
             sql = "INSERT INTO crypto." + message + " VALUES (" + (values-1)*"%s, " + "%s)"
             if op:
-                print(op.keys())
                 cursor.execute(sql, list(op.values()))
         my_db.commit()
     print(sep)
