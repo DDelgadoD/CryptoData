625320ae55533cfda146775f1062f84228fa9f32
David Delgado-Dueñas <35778364+Ddelgadu@users.noreply.github.com>
2021-11-13 15:23:15 +0100

    more than 6 months old deposits

--- src/tradesAndOrders.py
+++ src/tradesAndOrders.py
@@ -118,7 +118,6 @@ async def get_dividends(client, values=6):
             a = await binance_old_dividends(lending_type=lending_type, end_time=div_db-1)
 
             for op in tqdm(a):
-                print(op)
                 b = {'id': "000000", 'tranId': int(int(op['time'])*float(op["interest"])), 'asset': op['asset'], 'amount': op['interest'], 'divTime': op['time'], 'enInfo': 'OLD ' + op['lendingType']}
                 cursor.execute(sql, list(b.values()))
 
